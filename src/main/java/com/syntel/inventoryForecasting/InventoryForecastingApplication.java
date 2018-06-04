f7f322bedc7202a762ef5baed367b89760b9e1f5
syntel <huntmac10@gmail.com>
2018-06-04 15:20:57 -0400

    Bootstrapped React on SpringBoot web application and installed necessary libraries and modules

---
+++ src/main/java/com/syntel/inventoryForecasting/InventoryForecastingApplication.java
@@ -0,0 +1,12 @@
+package com.syntel.inventoryForecasting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryForecastingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryForecastingApplication.class, args);
+	}
+}
