12279ac02f7da13cd3e0273ef6da367360e4d42e
Jrod9599 <Jrod9599@gmail.com>
2018-06-08 12:22:28 -0400

    sending up spring stuff

--- src/main/java/com/syntel/inventoryForecasting/dao/ForecastDao.java
+++ src/main/java/com/syntel/inventoryForecasting/dao/ForecastDao.java
@@ -16,11 +16,39 @@ public class ForecastDao {
     JdbcTemplate jdbcTemplate;
 
     public List<QueryResult> getPastSales(SearchParam query) {
-        String sql = "SELECT sh_week AS week, sh_year AS year, f_description AS factor, sh_sku_id AS sku_id, sku_description AS description, sh_qty AS quantity FROM saleshistory, factors, skumaster WHERE sh_factor_id = f_id AND sh_sku_id = sku_id ORDER BY sh_year, sh_week LIMIT 1";
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(QueryResult.class));
-//        List<QueryResult> results = new ArrayList<>();
-//        QueryResult test = new QueryResult();
-//        results.add(test);
-//        return results;
+        String sqlSelect = "SELECT sh_week AS week, sh_year AS year, f_description AS factor, sh_sku_id AS sku_id, sku_description AS description, sh_qty AS quantity";
+        String sqlFrom = " FROM saleshistory, factors, skumaster";
+        String sqlWhere = " WHERE sh_factor_id = f_id AND sh_sku_id = sku_id";
+        String sqlConditions = "";
+
+        ArrayList<Object> arguments = new ArrayList<>();
+
+
+        //todo: add quotes around everything
+        if(query.getType() != ""){
+            sqlFrom += ", strmaster";
+            sqlConditions += " AND sh_str_id = str_id AND str_type = ?";
+            arguments.add(query.getType());
+        }
+        if(query.getStr() != ""){
+            sqlConditions += " AND sh_str_id = ?";
+            arguments.add(query.getStr());
+        }
+        if(query.getSku() != ""){
+            sqlConditions += " AND sh_sku_id = ?";
+            arguments.add(query.getSku());
+        }
+        if(query.getFactor() != ""){
+            sqlConditions += " AND f_description = ?";
+            arguments.add(query.getFactor());
+        }
+
+        //todo: need to figure out date logic
+        
+
+        String sqlOrder = " ORDER BY sh_year, sh_week";
+        String sql  = sqlSelect + sqlFrom + sqlWhere + sqlConditions + sqlOrder;
+
+        return jdbcTemplate.query(sql, arguments.toArray(), new BeanPropertyRowMapper<>(QueryResult.class));
     }
 }
