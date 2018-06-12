53ae782f6d04e816d4fa3314e8f8cb59b619a6bc
syntel <huntmac10@gmail.com>
2018-06-12 16:17:17 -0400

    Working on forecasting logic

--- src/main/java/com/syntel/inventoryForecasting/dao/ForecastDao.java
+++ src/main/java/com/syntel/inventoryForecasting/dao/ForecastDao.java
@@ -17,7 +17,7 @@ public class ForecastDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public List<Factors> getFactors(){
+    public List<Factors> getFactors() {
         String sql = "Select f_description FROM forecast_capstone.factors";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Factors.class));
     }
@@ -32,32 +32,35 @@ public class ForecastDao {
 
         ArrayList<Object> arguments = new ArrayList<>();
 
-        if(query.getType() != ""){
+        if (query.getType() != "") {
             sqlFrom += ", strmaster";
             sqlConditions += " AND sh_str_id = str_id AND str_type = ?";
             arguments.add(query.getType());
         }
-        if(query.getStr() != ""){
+        if (query.getStr() != "") {
             sqlConditions += " AND sh_str_id = ?";
             arguments.add(query.getStr());
         }
-        if(query.getSku() != ""){
+        if (query.getSku() != "") {
             sqlConditions += " AND sh_sku_id = ?";
             arguments.add(query.getSku());
         }
 
-        if(query.getWeekStart() != 0){ //both weeks were filled out or least the first week was filled
-            if(query.getYearStart() != 0){ //if both years exist
-                if(query.getYearStart() == query.getYearEnd()){ //if the same year
+        if (query.getWeekStart() != 0) { //both weeks were filled out or least the first week was filled
+            if (query.getYearStart() != 0) { //if both years exist
+                if (query.getYearStart() == query.getYearEnd()) { //if the same year
                     sqlConditions += " AND (sh_week >= ? AND sh_week <= ?) AND sh_year >= ?";
-                    arguments.add(query.getWeekStart()); arguments.add(query.getWeekEnd()); arguments.add(query.getYearStart());
-                }
-                else{//todo: range between years
+                    arguments.add(query.getWeekStart());
+                    arguments.add(query.getWeekEnd());
+                    arguments.add(query.getYearStart());
+                } else {//todo: range between years
                     sqlConditions += "  AND ((sh_week >= ? AND sh_year = ?) OR sh_year > ? )";
-                    arguments.add(query.getWeekStart()); arguments.add(query.getYearStart()); arguments.add(query.getYearStart());
+                    arguments.add(query.getWeekStart());
+                    arguments.add(query.getYearStart());
+                    arguments.add(query.getYearStart());
 
                     String outterSQL = "SELECT strPoint.* FROM (";
-                    String innerSQL  = sqlSelect + sqlFrom + sqlWhere + sqlConditions + sqlOrder;
+                    String innerSQL = sqlSelect + sqlFrom + sqlWhere + sqlConditions + sqlOrder;
                     outterSQL += innerSQL;
                     outterSQL += ") strPoint WHERE NOT (week > ? AND year >= ?)";
 
@@ -67,34 +70,31 @@ public class ForecastDao {
                     return jdbcTemplate.query(outterSQL, arguments.toArray(), new BeanPropertyRowMapper<>(QueryResult.class));
 
                 }
-            }
-            else if(query.getYearEnd() != 0){ //only filled the endyear box and not the start year
+            } else if (query.getYearEnd() != 0) { //only filled the endyear box and not the start year
                 sqlConditions += " AND sh_year = ?";
                 arguments.add(query.getYearEnd());
-            }
-            else{ //no year was filled out
+            } else { //no year was filled out
                 sqlConditions += " AND sh_week >= ? AND sh_week <= ? ";
-                arguments.add(query.getWeekStart()); arguments.add(query.getWeekEnd());
+                arguments.add(query.getWeekStart());
+                arguments.add(query.getWeekEnd());
             }
-        }
-        else if(query.getWeekEnd() != 0){
+        } else if (query.getWeekEnd() != 0) {
             sqlConditions += " AND sh_year = ?";
             arguments.add(query.getWeekEnd());
         }
 
-        if(query.getWeekStart() == 0 && query.getWeekEnd() == 0){
-                if(query.getYearStart() != 0){
-                    sqlConditions += " AND sh_year >= ? AND sh_year <= ?";
-                    arguments.add(query.getYearStart());
-                    arguments.add(query.getYearEnd());
-                }
-                else if(query.getYearEnd() != 0){
-                    sqlConditions += " AND sh_year = ?";
-                    arguments.add(query.getYearEnd());
-                }
+        if (query.getWeekStart() == 0 && query.getWeekEnd() == 0) {
+            if (query.getYearStart() != 0) {
+                sqlConditions += " AND sh_year >= ? AND sh_year <= ?";
+                arguments.add(query.getYearStart());
+                arguments.add(query.getYearEnd());
+            } else if (query.getYearEnd() != 0) {
+                sqlConditions += " AND sh_year = ?";
+                arguments.add(query.getYearEnd());
+            }
         }
 
-        String sql  = sqlSelect + sqlFrom + sqlWhere + sqlConditions +  sqlOrder;
+        String sql = sqlSelect + sqlFrom + sqlWhere + sqlConditions + sqlOrder;
 
         return jdbcTemplate.query(sql, arguments.toArray(), new BeanPropertyRowMapper<>(QueryResult.class));
     }
@@ -109,40 +109,38 @@ public class ForecastDao {
                 }
             }
 
-            int averageQty = 0;
-            if (cacheList.size() > 0) {
-                // todo: Check for weeks 1-3 and add to 49
-                if (cacheList.size() == 1) {
-                    List<QueryResult> weekRangeResults = new ArrayList<>();
-                    for (QueryResult weekResult : results) {
-                        if (weekResult.getWeek() <= i + 3 && weekResult.getWeek() >= i - 3) {
-                            weekRangeResults.add(weekResult);
-                        }
-                    }
-                    for (QueryResult weekResult : weekRangeResults) {
-                        averageQty += weekResult.getQuantity();
-                    }
-                    QueryResult weekForecast = new QueryResult();
-                    weekForecast.setSku_id(cacheList.get(0).getSku_id());
-                    weekForecast.setDescription(cacheList.get(0).getDescription());
-                    weekForecast.setWeek(cacheList.get(0).getWeek());
-                    weekForecast.setYear(2018);
-                    weekForecast.setFactor(searchParams.getFactor());
-                    weekForecast.setQuantity(averageQty / weekRangeResults.size());
-                    forecastResults.add(weekForecast);
-                } else {
-                    for (QueryResult weekResult : cacheList) {
-                        averageQty += weekResult.getQuantity();
-                    }
-                    QueryResult weekForecast = new QueryResult();
-                    weekForecast.setSku_id(cacheList.get(0).getSku_id());
-                    weekForecast.setDescription(cacheList.get(0).getDescription());
-                    weekForecast.setWeek(cacheList.get(0).getWeek());
-                    weekForecast.setYear(2018);
-                    weekForecast.setFactor(searchParams.getFactor());
-                    weekForecast.setQuantity(averageQty / cacheList.size());
-                    forecastResults.add(weekForecast);
+            if (cacheList.size() > 1) {
+                int averageQty = 0;
+                List<QueryResult> weekRangeResults = new ArrayList<>();
+//                    for (QueryResult weekResult : results) {
+//                        if (weekResult.getWeek() <= i + 3 && weekResult.getWeek() >= i - 3) {
+//                            weekRangeResults.add(weekResult);
+//                        }
+//                    }
+                for (QueryResult weekResult : cacheList) {
+                    averageQty += weekResult.getQuantity();
                 }
+                QueryResult weekForecast = new QueryResult();
+                weekForecast.setSku_id(cacheList.get(0).getSku_id());
+                weekForecast.setDescription(cacheList.get(0).getDescription());
+                weekForecast.setWeek(cacheList.get(0).getWeek());
+                weekForecast.setYear(2018);
+                weekForecast.setFactor(searchParams.getFactor());
+                weekForecast.setQuantity(averageQty / cacheList.size());
+                forecastResults.add(weekForecast);
+            } else if (cacheList.size() == 1) {
+                int averageQty = 0;
+                for (QueryResult weekResult : cacheList) {
+                    averageQty += weekResult.getQuantity();
+                }
+                QueryResult weekForecast = new QueryResult();
+                weekForecast.setSku_id(cacheList.get(0).getSku_id());
+                weekForecast.setDescription(cacheList.get(0).getDescription());
+                weekForecast.setWeek(cacheList.get(0).getWeek());
+                weekForecast.setYear(2018);
+                weekForecast.setFactor(searchParams.getFactor());
+                weekForecast.setQuantity(averageQty);
+                forecastResults.add(weekForecast);
             }
         }
         return forecastResults;
